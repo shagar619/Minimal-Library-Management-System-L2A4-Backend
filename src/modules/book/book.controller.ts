cc0e3f1c54e604e181017731a970c07e696ad2ff
shagar619 <ashagar619@gmail.com>
2025-10-01 12:57:47 +0600

    added get all books

--- src/modules/book/book.controller.ts
+++ src/modules/book/book.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { json, success, ZodError } from "zod";
 import { createBookSchema } from "../../validators/book.validators";
 import { Book } from "./book.model";
@@ -35,8 +35,42 @@ const createBook = async(req: Request, res: Response) => {
 };
 
 
+/**
+ * GET /books
+ * Query params:
+ *  - limit, skip, genre, author
+ */
+const getAllBooks = async(req: Request, res: Response, next: NextFunction) => {
+
+     try{
+          // get all books
+          const { limit = '100', skip = '0', genre, author } = req.query;
+          const q: any = {};
+          if (genre) q.genre = String(genre);
+          if (author) q.author = String(author);
+
+          const books = await Book.find(q)
+               .limit(Number(limit))
+               .skip(Number(skip))
+               .sort({ createdAt: -1 })
+               .exec();
+
+          res.status(201).json({
+               message: "Books retrieved successfully",
+               success: true,
+               books
+          });
+
+     } catch(error) {
+          next(error);
+     };
+};
+
+
+
 
 
 export const bookController = {
      createBook,
+     getAllBooks
 };
--- src/modules/book/book.route.ts
+++ src/modules/book/book.route.ts
@@ -5,6 +5,7 @@ import { bookController } from "./book.controller";
 const bookRoute = Router();
 
 bookRoute.post("/", bookController.createBook);
+bookRoute.get("/", bookController.getAllBooks);
 
 
 export default bookRoute;
